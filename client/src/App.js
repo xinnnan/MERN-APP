7a77bd7f0fee1573a5304001e824e7edde8d54b1
xinnnan <xinnancao@gmail.com>
2023-03-23 16:13:50 -0400

    barebone

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,11 @@ function App() {
         </nav>
 
       </header>
+      <div className="entry">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.peakpx.com%2Fen%2Fhd-wallpaper-desktop-fjrjy&psig=AOvVaw1kOlYhCjmiziyV1cmpZxkG&ust=1679688779936000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKDQ2bru8v0CFQAAAAAdAAAAABAE"></img>
+        <h2>Plant 1</h2>
+        <p></p>
+      </div>
     </main>
   );
 }
